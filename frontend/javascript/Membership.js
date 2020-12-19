2596c479bfa226f8b10acf0752ea20851396c19f
JoelleOribiana <75175212+JoelleOribiana@users.noreply.github.com>
2020-12-19 23:24:06 +0800

    Update and rename no to Membership.js

---
+++ frontend/javascript/Membership.js
@@ -0,0 +1,17 @@
+function openLogin() {
+  document.getElementById("Login").style.display = "block";
+  document.getElementById("Signup").style.display = "none"
+}
+
+function closeLogin() {
+  document.getElementById("Login").style.display = "none";
+}
+
+function openSignup() {
+  document.getElementById("Login").style.display = "none";
+  document.getElementById("Signup").style.display = "block";
+}
+
+function closeSignup() {
+  document.getElementById("Signup").style.display = "none";
+}
