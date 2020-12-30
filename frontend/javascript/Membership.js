7448542b533151d01da3b2f3c62edeffa659742a
idavnjx <usdavidniu@gmail.com>
2020-12-30 01:13:49 -0500

    Add Title; Footer; Adjust Login Opacity.

--- frontend/javascript/Membership.js
+++ frontend/javascript/Membership.js
@@ -1,10 +1,14 @@
 function openLogin() {
   document.getElementById("Login").style.display = "block";
   document.getElementById("Signup").style.display = "none"
+  document.getElementById("membership").style.opacity = "0.4";
+  document.getElementById("navigation").style.opacity = "0.4";
 }
 
 function closeLogin() {
   document.getElementById("Login").style.display = "none";
+  document.getElementById("membership").style.opacity = "1";
+  document.getElementById("navigation").style.opacity = "1";
 }
 
 function openSignup() {
