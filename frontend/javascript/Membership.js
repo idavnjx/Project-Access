e61fe04e22dad942e445ddb345abf906dc752b9b
idavnjx <usdavidniu@gmail.com>
2020-12-31 20:35:39 -0500

    Revamped index.html

--- frontend/javascript/Membership.js
+++ frontend/javascript/Membership.js
@@ -3,19 +3,27 @@ function openLogin() {
   document.getElementById("Signup").style.display = "none"
   document.getElementById("membership").style.opacity = "0.4";
   document.getElementById("navigation").style.opacity = "0.4";
+  document.getElementById("title").style.opacity = "0.4";
 }
 
 function closeLogin() {
   document.getElementById("Login").style.display = "none";
   document.getElementById("membership").style.opacity = "1";
   document.getElementById("navigation").style.opacity = "1";
+  document.getElementById("title").style.opacity = "1";
 }
 
 function openSignup() {
   document.getElementById("Login").style.display = "none";
   document.getElementById("Signup").style.display = "block";
+  document.getElementById("membership").style.opacity = "0.4";
+  document.getElementById("navigation").style.opacity = "0.4";
+  document.getElementById("title").style.opacity = "0.4";
 }
 
 function closeSignup() {
   document.getElementById("Signup").style.display = "none";
+  document.getElementById("membership").style.opacity = "1";
+  document.getElementById("navigation").style.opacity = "1";
+  document.getElementById("title").style.opacity = "1";
 }
